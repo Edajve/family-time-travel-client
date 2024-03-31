671c77b10374dc2b862d15a8b471e7730f6c7733
Dajve Echols <dajvelechols@gmail.com>
2024-03-31 17:57:41 -0500

    add desktop progile dropdown

--- src/scripts/navigation/Navigation.jsx
+++ src/scripts/navigation/Navigation.jsx
@@ -12,7 +12,16 @@ import {
     ButtonGroup,
     Image,
     VStack,
-    StackDivider
+    StackDivider,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+    PopoverCloseButton,
+
 } from '@chakra-ui/react'
 import { navigationStyles } from '../../styles/NavigationStyles'
 import {
@@ -23,14 +32,13 @@ import {
     DrawerContent,
     DrawerCloseButton,
 } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
-import { wrap } from 'framer-motion'
+import { Card, CardBody, CardFooter } from '@chakra-ui/react'
 
 
 export const Navigation = () => {
     const theme = useTheme();
     const styles = navigationStyles(theme);
-    const { isOpen, onOpen, onClose } = useDisclosure()
+    const { isOpen: isMobileOpen, onOpen: onMobileOpen, onClose: onMobileClose } = useDisclosure();
 
     const drawerStyle = {
         navigationHeader: {
@@ -117,94 +125,119 @@ export const Navigation = () => {
             <Box className='mobileNavContainer' sx={styles.mobileNavContainer}>
                 {/* Mobile Hamburger Icon */}
                 <Box sx={styles.hamburgerIconContainer} className='hamburgerIconContainer'>
-                    <Button className='hamburgerButton' sx={styles.hamburgerButton} variant='link' onClick={onOpen}>
+                    <Button className='hamburgerButton' sx={styles.hamburgerButton} variant='link' onClick={onMobileOpen}>
                         <HamburgerIcon boxSize={9} sx={styles.hamburgerButton} />
+                        <Drawer onClose={onMobileClose} isOpen={isMobileOpen} size='xs'>
+                            <DrawerOverlay />
+                            <DrawerContent>
+                                <DrawerCloseButton className='navigationDrawerCloseButton' sx={drawerStyle.navigationDrawerCloseButton} />
+                                <DrawerHeader as='span' className='navigationHeader' sx={drawerStyle.navigationHeader}> {`Account`}</DrawerHeader>
+                                <DrawerBody className='navigationDrawerBody' sx={drawerStyle.navigationDrawerBody}>
+                                    <Card className='navigationCardContainer' sx={drawerStyle.navigationCardContainer} maxW='sm'>
+                                        <CardBody className='navigationCardBody' sx={drawerStyle.navigationCardBody}>
+                                            <Image
+                                                className='navigationCardImage'
+                                                src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                                                alt='Green double couch with wooden legs'
+                                                borderRadius='lg'
+                                            />
+                                            <Stack className='navigationCardStace' sx={drawerStyle.navigationCardStace} mt='6' spacing='3'>
+                                                <Heading className='navigationCardHeading' sx={drawerStyle.navigationCardHeading} size='md'>Member</Heading>
+                                                <Text className='navigationInfoText' sx={drawerStyle.navigationInfoText}>
+                                                    Information
+                                                </Text>
+                                                <VStack
+                                                    className='navigationCardInformationContainer'
+                                                    sx={drawerStyle.navigationCardInformationContainer}
+                                                    color='blue.600' fontSize='2xl'
+                                                    divider={<StackDivider borderColor='gray.200' />}
+                                                    spacing={1}
+                                                    align='stretch'
+                                                >
+                                                    <Box className='navigationCardInfoSectionOne' sx={drawerStyle.navigationCardInfoSectionOne} h='40px'>
+                                                        <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
+                                                            Name
+                                                        </Box>
+                                                        <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
+                                                            James Bond
+                                                        </Box>
+                                                    </Box>
+                                                    <Box className='navigationCardInfoSectionTwo' sx={drawerStyle.navigationCardInfoSectionTwo} h='40px' >
+                                                        <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
+                                                            Email
+                                                        </Box>
+                                                        <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
+                                                            TestEmail.gmail-test.com
+                                                        </Box>
+                                                    </Box>
+                                                    <Box className='navigationCardInfoSectionThree' sx={drawerStyle.navigationCardInfoSectionThree} h='40px' >
+                                                        <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
+                                                            Phone
+                                                        </Box>
+                                                        <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
+                                                            777-777-7777
+                                                        </Box>
+                                                    </Box>
+                                                    <Box className='navigationCardInfoSectionThree' sx={drawerStyle.navigationCardInfoSectionThree} h='40px' >
+                                                        <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
+                                                            Joimed
+                                                        </Box>
+                                                        <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
+                                                            March 25th, 2021
+                                                        </Box>
+                                                    </Box>
+
+                                                </VStack>
+                                            </Stack>
+                                        </CardBody>
+                                        <Divider />
+                                        <CardFooter className='navigationCardFooter' sx={drawerStyle.navigationCardFooter}>
+                                            <ButtonGroup className='navigationCardButtonGroup' sx={drawerStyle.navigationCardButtonGroup} spacing='2'>
+                                                <Button size={4} className='navigationCardButtoneOne' sx={drawerStyle.navigationCardButtoneOne} variant='solid' colorScheme='blue'>
+                                                    Settings
+                                                </Button>
+                                                <Button size='20px' className='navigationCardButtonTwo' sx={drawerStyle.navigationCardButtonTwo} variant='ghost' colorScheme='blue'>
+                                                    Add Time Capsule
+                                                </Button>
+                                            </ButtonGroup>
+                                        </CardFooter>
+                                    </Card>
+                                </DrawerBody>
+                            </DrawerContent>
+                        </Drawer>
                     </Button >
                 </Box >
                 {/* Laptup and up navigation elements */}
                 <Box sx={styles.desktopAccountDropDown} className='desktopAccountDropwodn'>
-                    Hello
+                    <Popover
+                        placement='bottom'
+                        closeOnBlur={false}
+                    >
+                        <PopoverTrigger>
+                            <Button className='desktopAccountButton' sx={styles.desktopAccountButton}>Account</Button>
+                        </PopoverTrigger>
+                        <PopoverContent className='desktopAccountPopUp' sx={styles.desktopAccountPopUp} >
+                            <PopoverHeader pt={4} fontWeight='bold' border='0'>
+                                Hey James
+                            </PopoverHeader>
+                            <PopoverArrow className='desktopPopUpArrow' sx={styles.desktopAccountPopUpArrow} />
+                            <PopoverCloseButton />
+                            <PopoverFooter
+                                border='0'
+                                display='flex'
+                                alignItems='center'
+                                justifyContent='space-between'
+                                pb={4}
+                            >
+
+                                <ButtonGroup size='sm'>
+                                    <Button className='desktopAccountSettingsButton' sx={styles.desktopAccountSettingsButton} colorScheme='green'>Account Settings</Button>
+                                </ButtonGroup>
+                            </PopoverFooter>
+                        </PopoverContent>
+                    </Popover>
                 </Box>
             </Box>
-
-            <Drawer onClose={onClose} isOpen={isOpen} size='xs'>
-                <DrawerOverlay />
-                <DrawerContent>
-                    <DrawerCloseButton className='navigationDrawerCloseButton' sx={drawerStyle.navigationDrawerCloseButton} />
-                    <DrawerHeader as='span' className='navigationHeader' sx={drawerStyle.navigationHeader}> {`Account`}</DrawerHeader>
-                    <DrawerBody className='navigationDrawerBody' sx={drawerStyle.navigationDrawerBody}>
-                        <Card className='navigationCardContainer' sx={drawerStyle.navigationCardContainer} maxW='sm'>
-                            <CardBody className='navigationCardBody' sx={drawerStyle.navigationCardBody}>
-                                <Image
-                                    className='navigationCardImage'
-                                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                                    alt='Green double couch with wooden legs'
-                                    borderRadius='lg'
-                                />
-                                <Stack className='navigationCardStace' sx={drawerStyle.navigationCardStace} mt='6' spacing='3'>
-                                    <Heading className='navigationCardHeading' sx={drawerStyle.navigationCardHeading} size='md'>Member</Heading>
-                                    <Text className='navigationInfoText' sx={drawerStyle.navigationInfoText}>
-                                        Information
-                                    </Text>
-                                    <VStack
-                                        className='navigationCardInformationContainer'
-                                        sx={drawerStyle.navigationCardInformationContainer}
-                                        color='blue.600' fontSize='2xl'
-                                        divider={<StackDivider borderColor='gray.200' />}
-                                        spacing={1}
-                                        align='stretch'
-                                    >
-                                        <Box className='navigationCardInfoSectionOne' sx={drawerStyle.navigationCardInfoSectionOne} h='40px'>
-                                            <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
-                                                Name
-                                            </Box>
-                                            <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
-                                                James Bond
-                                            </Box>
-                                        </Box>
-                                        <Box className='navigationCardInfoSectionTwo' sx={drawerStyle.navigationCardInfoSectionTwo} h='40px' >
-                                            <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
-                                                Email
-                                            </Box>
-                                            <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
-                                                TestEmail.gmail-test.com
-                                            </Box>
-                                        </Box>
-                                        <Box className='navigationCardInfoSectionThree' sx={drawerStyle.navigationCardInfoSectionThree} h='40px' >
-                                            <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
-                                                Phone
-                                            </Box>
-                                            <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
-                                                777-777-7777
-                                            </Box>
-                                        </Box>
-                                        <Box className='navigationCardInfoSectionThree' sx={drawerStyle.navigationCardInfoSectionThree} h='40px' >
-                                            <Box className='navigationCardInfoTemplate' sx={drawerStyle.navigationCardInfoTemplate} h='40px'>
-                                                Joimed
-                                            </Box>
-                                            <Box className='navigationCardInfoSectionValue' sx={drawerStyle.navigationCardInfoSectionValue} h='40px'>
-                                                March 25th, 2021
-                                            </Box>
-                                        </Box>
-
-                                    </VStack>
-                                </Stack>
-                            </CardBody>
-                            <Divider />
-                            <CardFooter className='navigationCardFooter' sx={drawerStyle.navigationCardFooter}>
-                                <ButtonGroup className='navigationCardButtonGroup' sx={drawerStyle.navigationCardButtonGroup} spacing='2'>
-                                    <Button size={4} className='navigationCardButtoneOne' sx={drawerStyle.navigationCardButtoneOne} variant='solid' colorScheme='blue'>
-                                        Settings
-                                    </Button>
-                                    <Button size='20px' className='navigationCardButtonTwo' sx={drawerStyle.navigationCardButtonTwo} variant='ghost' colorScheme='blue'>
-                                        Add Time Capsule
-                                    </Button>
-                                </ButtonGroup>
-                            </CardFooter>
-                        </Card>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
         </>
     )
 }
--- src/styles/NavigationStyles.js
+++ src/styles/NavigationStyles.js
@@ -1,24 +1,50 @@
 export const navigationStyles = (theme) => ({
-    hamburgerIconContainer: {
-        height: '8vh'
-        , width: '100vw'
-        , padding: theme.space.md
-        , display: 'flex'
-        , justifyContent: 'flex-end'
-        , '@media screen and (min-width: 768px)': {
-            display: 'none'
-          }
-    }
-    , hamburgerButton: {
-        color: theme.colors.textMain,
-    }
-    , desktopAccountDropDown : {
+  hamburgerIconContainer: {
+    height: '8vh'
+    , width: '100vw'
+    , padding: theme.space.md
+    , display: 'flex'
+    , justifyContent: 'flex-end'
+    , '@media screen and (min-width: 768px)': {
       display: 'none'
-      , '@media screen and (min-width: 768px)': {
-        display: 'inline'
-      }
-    , 
     }
+  }
+  , hamburgerButton: {
+    color: theme.colors.textMain,
+  }
+  , desktopAccountDropDown: {
+    marginLeft: 'auto'
+    , width: '126px'
+  }
+  , desktopAccountButton: {
+    color: theme.colors.textMain
+    , fontWeight: '200'
+    , backgroundColor: theme.colors.background
+    , marginLeft: 'auto'
+    , display: 'none'
+    , marginTop: theme.space.md
+    , _hover: {
+      backgroundColor: theme.colors.background,
+      color: theme.colors.textMainDeemphasize
+    }
+    , '@media screen and (min-width: 768px)': {
+      display: 'inline'
+    }
+  }
+  , desktopAccountPopUp: {
+    color: theme.colors.background
+    , backgroundColor: theme.colors.textMainDeemphasize
+    , burderColor: theme.colors.textMainDeemphasize
+    , marginRight: theme.space.md
+  }
+  , desktopAccountSettingsButton: {
+    background: theme.colors.secondary
+    , color: theme.colors.background
+    , _hover: {
+      backgroundColor: theme.colors.background,
+      color: theme.colors.secondary
+    }
+  }
 });
 
 export default navigationStyles;
