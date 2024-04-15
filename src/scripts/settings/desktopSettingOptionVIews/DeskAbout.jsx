dd77d8d35edf9e5497b488862c8e69f4b4345ea6
Dajve Echols <dajvelechols@gmail.com>
2024-04-14 21:01:16 -0500

    setting up about page

--- src/scripts/settings/desktopSettingOptionVIews/DeskAbout.jsx
+++ src/scripts/settings/desktopSettingOptionVIews/DeskAbout.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import DeskAboutStyles from '../../../styles/DeskAboutStyles'
+import {
+  useTheme
+  , Box
+  , Text
+} from '@chakra-ui/react';
 
 const DeskAbout = () => {
+  const theme = useTheme();
+  const styles = DeskAboutStyles(theme);
+
   return (
-    <div>DeskAbout</div>
+    <Box className='aboutContainer' sx={styles.aboutContainer}>
+      <Text className='aboutHeader' sx={styles.aboutHeader}>Why the website?</Text>
+      <Text className='aboutParagraph' sx={styles.aboutParagraph}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Officiis placeat necessitatibus molestiae. In ipsam iusto reiciendis
+        ullam accusamus, maxime tenetur aliquam, consectetur, fuga magni impedit
+        est quisquam blanditiis distinctio. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Accusamus voluptas doloremque rem culpa est, sunt
+        dignissimos laboriosam qui, voluptates in aperiam corrupti temporibus
+        deleniti nemo repellendus corporis omnis, debitis dolorem!
+      </Text>
+    </Box>
   )
 }
 
---
+++ src/styles/DeskAboutStyles.js
@@ -0,0 +1,19 @@
+export const DeskAboutStyles = (theme) => ({
+    aboutContainer: {
+        width: '100%'
+        , height: '100%'
+        , padding: theme.space.lg
+    }
+    , aboutHeader: {
+        fontSize: '24px'
+        , fontWeight: 'bold'
+        , marginBottom: '15px'
+    },
+    aboutParagraph: {
+        fontSize: '16px'
+        , lineHeight: '1.6'
+        , marginLeft: theme.space.md
+    },
+})
+
+export default DeskAboutStyles
