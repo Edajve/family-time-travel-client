473b240f08e6ca8c6b45b8152136c21dec1dbdac
Dajve Echols <dajvelechols@gmail.com>
2024-04-15 20:52:57 -0500

    configure front page grid

--- src/scripts/Home.jsx
+++ src/scripts/Home.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import { homeStyles } from "../styles/HomeStyles"
 import { useTheme, Box } from '@chakra-ui/react'
-import Navigation from './navigation/Navigation';
-import { NoteBody } from './NoteBody';
+import Navigation from './navigation/Navigation'
+import { homeStyles } from "../styles/HomeStyles"
 
 export const Home = () => {
-  const theme = useTheme();
-  const styles = homeStyles(theme);
+    const theme = useTheme();
+    const styles = homeStyles(theme);
 
-  return (
-    <Box sx={styles.homeContainer} className='homeContainer'>
-      <Navigation />
-      {/* <NoteBody /> */}
-      {/* in the home body is where you would be able to link the not body */}
-      <Box sx={styles.homeBodyContainer} className='homeBodyContainer'>
-
-      </Box>
-    </Box>
-  )
+    return (
+        <Box sx={styles.homeContainer} className='homeContainer'>
+            <Navigation />
+            <Box sx={styles.homeBodyContainer} className='homeBodyContainer'>
+                <Box sx={styles.homeBodyInformationGrid} className='homeBodyInformationGrid'></Box>
+                <Box sx={styles.homeBodyCardGrid} className='homeBodyCardGrid'>
+                    <Box sx={styles.homeBodyLargeCard} className='homeBodyLargeCard'>large</Box>
+                    <Box sx={styles.homeBodyMediumCard} className='homeBodyMediumCard'>medium</Box>
+                    <Box sx={styles.homeBodySmallCard} className='homeBodySmallCard'>small</Box>
+                    <Box sx={styles.homeBodyLargeCard} className='homeBodyLargeCard'>large</Box>
+                    <Box sx={styles.homeBodyMediumCard} className='homeBodyMediumCard'>medium</Box>
+                </Box>
+            </Box>
+        </Box>
+    )
 }
 
-export default Home
+export default Home;
--- src/styles/HomeStyles.js
+++ src/styles/HomeStyles.js
@@ -1,15 +1,47 @@
 export const homeStyles = (theme) => ({
     homeContainer: {
-        height: '100vh',
-        width: '100vw',
-        background: `radial-gradient(circle at top right, #D7C2C9 -100%, #283d36 70%)`
-    },
-    hamburgerIcon: {
-        textAlign: 'end'
-    },
-    homeBodyContainer: {
+        height: '100vh'
+        , width: '100vw'
+        , background: `radial-gradient(circle at center right, #D7C2C9 -200%, #283d36 50%)`,
+    }
+    , hamburgerIcon: {
+        textAlign: 'end',
+    }
+    , homeBodyContainer: {
 
     }
-});
-
-export default homeStyles;
+    , homeBodyInformationGrid: {
+        width: '100%'
+        , height: '300px'
+        , borderBottom: '1px solid white',
+    }
+    , homeBodyCardGrid: {
+        display: 'flex'
+        , flexWrap: 'wrap'
+        , justifyContent: 'space-evenly'
+        , alignItems: 'flex-start'
+        , gap: '15px'
+        , padding: '15px',
+    }
+    , homeBodyLargeCard: {
+        flex: '1 1 400px'
+        , height: '250px'
+        , border: '0.5px solid #c0ccc897'
+        , backgroundColor: theme.colors.background
+        , borderRadius: theme.space.sm,
+    }
+    , homeBodyMediumCard: {
+        flex: '1 1 300px'
+        , height: '250px'
+        , border: '0.5px solid #c0ccc897'
+        , backgroundColor: theme.colors.background
+        , borderRadius: theme.space.sm,
+    }
+    , homeBodySmallCard: {
+        flex: '1 1 200px'
+        , height: '250px'
+        , border: '0.5px solid #c0ccc897'
+        , backgroundColor: theme.colors.background
+        , borderRadius: theme.space.sm,
+    },
+})
