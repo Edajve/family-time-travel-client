477aff0e3465afca8c53eff481d9903328fb25be
Dajve Echols <dajvelechols@gmail.com>
2024-04-18 23:23:00 -0500

    add back routing from any page component

---
+++ src/scripts/navigation/BackHeader.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, useTheme } from '@chakra-ui/react'
+import { Link } from "react-router-dom"
+import BackHeaderStyles from '../../styles/BackHeaderStyles'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+
+const BackHeader = () => {
+    const theme = useTheme()
+    const styles = BackHeaderStyles(theme)
+
+    return (
+        <Box className='backHeaderContainer' sx={styles.backHeaderContainer}>
+            <Link to="/home">
+                <ChevronLeftIcon boxSize={10} className='backHeader' sx={styles.backHeader} />
+            </Link>
+        </Box>
+    )
+}
+
+export default BackHeader
---
+++ src/styles/BackHeaderStyles.js
@@ -0,0 +1,12 @@
+export const BackHeaderStyles = (theme) => ({
+    backHeaderContainer: {
+        width: '100%'
+        , height: '70px'
+    }
+    , backHeader: {
+        color: theme.colors.textMain
+        , marginTop: '10px'
+    }
+})
+
+export default BackHeaderStyles
