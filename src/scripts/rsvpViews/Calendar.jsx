a1deabe0eab0331aee8eceba61196106e7ab3e74
Dajve Echols <dajvelechols@gmail.com>
2024-04-21 13:11:11 -0500

    conditionally render so that the desktop header and such does not show on rsvp page

--- src/scripts/rsvpViews/Calendar.jsx
+++ src/scripts/rsvpViews/Calendar.jsx
@@ -21,8 +21,6 @@ import MobileCalendar from "./MobileCalendar.jsx";
 import {useEffect, useState} from "react";
 import DesktopCalendarAside from "./DesktopCalendarAside.jsx";
 
-import DesktopCalendar from "./DesktopCalendarGrid.jsx";
-
 const Calendar = () => {
     const theme = useTheme();
     const styles = CalendarStyles(theme);
@@ -47,58 +45,60 @@ const Calendar = () => {
     return (
         <HStack className='calendarDesktopAndMobileContainer' gap={0} sx={styles.calendarDesktopAndMobileContainer}>
             {isWindowOver768px && <DesktopCalendarAside/>}
-            {/* VStack Header for both mobile and desktop calendars*/}
-            <VStack className='calendarMobileContainer' sx={styles.calendarMobileContainer}>
-                <HStack className='calendarHeader' sx={styles.calendarHeader}>
-                    <Box className='calendarHeaderDate' sx={styles.calendarHeaderDate}>
-                        <Text className='calendarHeaderDateElement' sx={styles.calendarHeaderDateElement}>
-                            Fri April 19, 2024
-                        </Text>
-                    </Box>
-                    <HStack className='calendarHeaderButtonsContainer' sx={styles.calendarHeaderButtonsContainer}>
-                        <ButtonGroup className='calendarHeaderButtonGroup' sx={styles.calendarHeaderButtonGroup}
-                                     variant='outline' spacing='6'>
-                            <Button size={2} onClick={() => handleDateChangeClick("left")}
-                                    className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
-                                    variant='ghost'>
-                                <ChevronLeftIcon boxSize={6}/>
-                            </Button>
-                            <Button size={2} onClick={() => handleDateChangeClick("right")}
-                                    className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
-                                    variant='ghost'>
-                                <ChevronRightIcon boxSize={6}/>
-                            </Button>
-                        </ButtonGroup>
-                        {!isWindowOver768px &&
-                            <Box className='calendarHeaderHamburgerIcon' sx={styles.calendarHeaderHamburgerIcon}>
-                                <Menu>
-                                    <MenuButton
-                                        className='calendarHeaderHamburger'
-                                        sx={styles.calendarHeaderHamburger}
-                                        as={IconButton}
-                                        aria-label='Options'
-                                        icon={<HamburgerIcon/>}
-                                        variant='ghost'
-                                    />
-                                    <MenuList className='calendarHeaderHamburgerMenuList'
-                                              sx={styles.calendarHeaderHamburgerMenuList}>
-                                        <Link to="/rsvp/create">
-                                            <MenuItem className='calendarHeaderHamburgerMenuItem'
-                                                      sx={styles.calendarHeaderHamburgerMenuItem} icon={<AddIcon/>}
-                                                      command='⌘T'>
-                                                Add Event
-                                            </MenuItem>
-                                        </Link>
-                                    </MenuList>
-                                </Menu>
-                            </Box>}
+            {!isWindowOver768px && (
+                <VStack className='calendarMobileContainer' sx={styles.calendarMobileContainer}>
+                    <HStack className='calendarHeader' sx={styles.calendarHeader}>
+                        <Box className='calendarHeaderDate' sx={styles.calendarHeaderDate}>
+                            <Text className='calendarHeaderDateElement' sx={styles.calendarHeaderDateElement}>
+                                Fri December 19, 2024
+                            </Text>
+                        </Box>
+                        <HStack className='calendarHeaderButtonsContainer' sx={styles.calendarHeaderButtonsContainer}>
+                            <ButtonGroup className='calendarHeaderButtonGroup' sx={styles.calendarHeaderButtonGroup}
+                                         variant='outline' spacing='6'>
+                                <Button size={2} onClick={() => handleDateChangeClick("left")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ChevronLeftIcon boxSize={6}/>
+                                </Button>
+                                <Button size={2} onClick={() => handleDateChangeClick("right")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ChevronRightIcon boxSize={6}/>
+                                </Button>
+                            </ButtonGroup>
+                            {!isWindowOver768px && (
+                                <Box className='calendarHeaderHamburgerIcon' sx={styles.calendarHeaderHamburgerIcon}>
+                                    <Menu>
+                                        <MenuButton
+                                            className='calendarHeaderHamburger'
+                                            sx={styles.calendarHeaderHamburger}
+                                            as={IconButton}
+                                            aria-label='Options'
+                                            icon={<HamburgerIcon/>}
+                                            variant='ghost'
+                                        />
+                                        <MenuList className='calendarHeaderHamburgerMenuList'
+                                                  sx={styles.calendarHeaderHamburgerMenuList}>
+                                            <Link to="/rsvp/create">
+                                                <MenuItem className='calendarHeaderHamburgerMenuItem'
+                                                          sx={styles.calendarHeaderHamburgerMenuItem} icon={<AddIcon/>}
+                                                          command='⌘T'>
+                                                    Add Event
+                                                </MenuItem>
+                                            </Link>
+                                        </MenuList>
+                                    </Menu>
+                                </Box>
+                            )}
+                        </HStack>
                     </HStack>
-                </HStack>
-                {/*This renders on UI when the vw is under 600px*/}
-                <MobileCalendar/>
-                <Divider className='calendarDivider' sx={styles.calendarDivider}/>
-                {isWindowOver768px ? (<DesktopCalendar/>) : (<MobileCalendarTracker/>)}
-            </VStack>
+                    {/*This renders on UI when the vw is under 600px*/}
+                    <MobileCalendar/>
+                    <Divider className='calendarDivider' sx={styles.calendarDivider}/>
+                    {!isWindowOver768px ? <MobileCalendarTracker/> : <Text>Something here</Text>}
+                </VStack>
+            )}
         </HStack>
     );
 }
