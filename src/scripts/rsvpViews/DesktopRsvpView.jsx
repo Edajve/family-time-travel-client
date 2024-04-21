5168edda7a1ad6a4d370e5d69acaf46c5af9f9a9
Dajve Echols <dajvelechols@gmail.com>
2024-04-21 18:59:56 -0500

    move state to parent rsvp component

---
+++ src/scripts/rsvpViews/DesktopRsvpView.jsx
@@ -0,0 +1,114 @@
+import {
+    Box,
+    Button,
+    ButtonGroup,
+    HStack,
+    Input,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Text,
+    useTheme
+} from "@chakra-ui/react";
+import CalendarStyles from "../../styles/CalendarStyles.js";
+import {ArrowLeftIcon, ArrowRightIcon, ChevronDownIcon, ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
+import {Link} from "react-router-dom";
+import DesktopCalendarGrid from "./DesktopCalendarGrid.jsx";
+import {useState} from "react";
+import CalendarUtils from "../globals/Time.js";
+
+// eslint-disable-next-line react/prop-types
+const DesktopRsvpView = ({currentTime, handleDateChangeClick}) => {
+    const theme = useTheme();
+    const styles = CalendarStyles(theme);
+    const [filterSearch, setFilterSearch] = useState("");
+    const timeUtils = new CalendarUtils();
+
+    function handleFilterSearch(input) {
+        setFilterSearch(input.target.value);
+    }
+
+    return (
+        <>
+            <Box className='calendarDesktopAsideContainer' sx={styles.calendarDesktopAsideContainer}>
+                <Box className='calendarAsideNewEventButton' sx={styles.calendarAsideNewEventButton}>
+                    <Menu>
+                        <MenuButton
+                            className='calendarAsideNewEventButtonMenuButton'
+                            sx={styles.calendarAsideNewEventButtonMenuButton}
+                            px={4}
+                            py={2}
+                            variant='ghost'
+                            transition='all 0.2s'
+                            borderRadius='md'
+                            borderWidth='1px'
+                            _hover={{bg: 'gray.400'}}
+                            _expanded={{bg: 'blue.400'}}
+                            _focus={{boxShadow: 'outline'}}
+                        >
+                            <Text className='calendarAsideNewEventText' sx={styles.calendarAsideNewEventText}>
+                                Create <ChevronDownIcon/>
+                            </Text>
+                        </MenuButton>
+                        <MenuList className='calendarAsideNewEventButtonMenuList'
+                                  sx={styles.calendarAsideNewEventButtonMenuList}>
+                            <Link to={'/rsvp/create'}>
+                                <MenuItem className='calendarAsideNewEventButtonMenuItem'
+                                          sx={styles.calendarAsideNewEventButtonMenuItem}>New RSVP</MenuItem>
+                            </Link>
+                        </MenuList>
+                    </Menu>
+                </Box>
+                <Box>
+                    <HStack className='calendarHeader' sx={styles.calendarHeader}>
+                        <HStack className='calendarHeaderButtonsContainer' sx={styles.calendarHeaderButtonsContainer}>
+                            <ButtonGroup className='calendarHeaderButtonGroup' sx={styles.calendarHeaderButtonGroup}
+                                         variant='outline' spacing='6'>
+                                <Button size={1} onClick={() => handleDateChangeClick("previous year")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ArrowLeftIcon boxSize={2}/>
+                                </Button>
+                                <Button size={1} onClick={() => handleDateChangeClick("previous month")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ChevronLeftIcon boxSize={3}/>
+                                </Button>
+                                <Box className='calendarHeaderDate' sx={styles.calendarHeaderDate}>
+                                    <Text className='calendarHeaderDateElement' sx={styles.calendarHeaderDateElement}>
+                                        {timeUtils.getUIReadyDate(currentTime)}
+                                    </Text>
+                                </Box>
+                                <Button size={1} onClick={() => handleDateChangeClick("next month")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ChevronRightIcon boxSize={3}/>
+                                </Button>
+                                <Button size={1} onClick={() => handleDateChangeClick("next year")}
+                                        className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                        variant='ghost'>
+                                    <ArrowRightIcon boxSize={2}/>
+                                </Button>
+                            </ButtonGroup>
+                        </HStack>
+                    </HStack>
+                    <DesktopCalendarGrid/>
+                </Box>
+                <Box className='calendarAsideFilterContainer' sx={styles.calendarAsideFilterContainer}>
+                    <Input
+                        className='calendarAsideFilterInput'
+                        sx={styles.calendarAsideFilterInput}
+                        size='sm'
+                        variant='outline'
+                        placeholder='Filter Events'
+                        value={filterSearch}
+                        onChange={handleFilterSearch}
+                    />
+                </Box>
+            </Box>
+            <Box><Text>Main Styles for Desktop View</Text></Box>
+        </>
+    )
+}
+export default DesktopRsvpView;
---
+++ src/scripts/rsvpViews/MobileRsvpView.jsx
@@ -0,0 +1,63 @@
+import {Box, Button, ButtonGroup, Divider, HStack, Menu, Text, useTheme, VStack} from "@chakra-ui/react";
+import {ArrowLeftIcon, ArrowRightIcon, ChevronLeftIcon, ChevronRightIcon} from "@chakra-ui/icons";
+import MobileCalendar from "./MobileCalendar.jsx";
+import MobileCalendarTracker from "./MobileCalendarTracker.jsx";
+import CalendarUtils from "../globals/Time.js";
+import CalendarStyles from "../../styles/CalendarStyles.js";
+
+// figure out why this prop is not coming through
+// eslint-disable-next-line react/prop-types
+const MobileRsvpView = ({currentTime, handleDateChangeClick}) => {
+    const theme = useTheme();
+    const styles = CalendarStyles(theme);
+    const timeUtils = new CalendarUtils();
+
+    return (
+        <VStack className='calendarMobileContainer' sx={styles.calendarMobileContainer}>
+            <HStack className='calendarHeader' sx={styles.calendarHeader}>
+                <Box className='calendarHeaderDate' sx={styles.calendarHeaderDate}>
+                    <Text className='calendarHeaderDateElement' sx={styles.calendarHeaderDateElement}>
+                        {timeUtils.getUIReadyDate(currentTime)}
+                    </Text>
+                </Box>
+
+                <HStack className='calendarHeaderButtonsContainer' sx={styles.calendarHeaderButtonsContainer}>
+                    <ButtonGroup className='calendarHeaderButtonGroup' sx={styles.calendarHeaderButtonGroup}
+                                 variant='outline' spacing='6'>
+                        <Button size={2} onClick={() => handleDateChangeClick("previous year")}
+                                className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                variant='ghost'>
+                            <ArrowLeftIcon boxSize={3}/>
+                        </Button>
+                        <Button size={2} onClick={() => handleDateChangeClick("previous month")}
+                                className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                variant='ghost'>
+                            <ChevronLeftIcon boxSize={6}/>
+                        </Button>
+                        <Button size={2} onClick={() => handleDateChangeClick("next month")}
+                                className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                variant='ghost'>
+                            <ChevronRightIcon boxSize={6}/>
+                        </Button>
+                        <Button size={2} onClick={() => handleDateChangeClick("next year")}
+                                className='calendarHeaderDateButtons' sx={styles.calendarHeaderDateButtons}
+                                variant='ghost'>
+                            <ArrowRightIcon boxSize={3}/>
+                        </Button>
+                    </ButtonGroup>
+                    <Box className='calendarHeaderHamburgerIcon' sx={styles.calendarHeaderHamburgerIcon}>
+                        <Menu>
+                            {/* Other JSX components */}
+                        </Menu>
+                    </Box>
+                </HStack>
+
+            </HStack>
+            <MobileCalendar/>
+            <Divider className='calendarDivider' sx={styles.calendarDivider}/>
+            <MobileCalendarTracker/>
+        </VStack>
+    )
+}
+
+export default MobileRsvpView;
